53cfbfb1a09de0f603854173011486a542eb2d8c
gunj-tibrewala <gunjtibrewala@gmail.com>
2026-05-02 23:56:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // This file acts as environment variables for the frontend
-const API_BASE_URL = "https://events-remainder-app-backend-p7r0r27zs-gunj-tibrewalas-projects.vercel.app";
+const API_BASE_URL = "https://events-remainder-app-backend.vercel.app";
